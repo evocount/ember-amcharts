1cf7f0fa6e2c42c5c03f38245011b86cdc4ee71b
Till Theato <till.theato@evocount.de>
2020-03-30 22:58:22 +0200

    Small cleanup.

--- tests/integration/components/am-chart-test.js
+++ tests/integration/components/am-chart-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, waitFor } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | am-chart', function(hooks) {
@@ -49,8 +49,7 @@ module('Integration | Component | am-chart', function(hooks) {
       </AmChart>
     `);
 
-    // TODO properly await loading of chart
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await waitFor('svg', { timeout: 2000 });
 
     assert.equal(
       this.element.querySelectorAll('svg').length,
